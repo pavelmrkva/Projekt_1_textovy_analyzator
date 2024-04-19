e32599c05da7c6d4fe41b4c9403c863cac22bf5e
mr.Pavel <160313016+pavelmrkva@users.noreply.github.com>
2024-04-19 15:44:03 +0200

    Update textovy_analyzator.py

--- textovy_analyzator.py
+++ textovy_analyzator.py
@@ -85,18 +85,12 @@ number_count = 0
 number_sum = 0
 
 for word in analyze:
-    
-    if word.istitle() and word.isalpha():
+    if word.istitle():
         title_case += 1
-       
     elif word.isupper() and word.isalpha():
         upper_case += 1
-        
-
-    elif word.islower() and word.isalpha():
+    elif word.islower():
         lower_case += 1
-        
-
     elif word.isnumeric():
         number_count += 1
         number_sum += int(word)
